ebd6f495cd2a9a2dd5a322de7d759ea8a4664069
有佳 小野瀬 <yuuka.082508250825@gmail.com>
2023-05-23 14:44:33 +0900

    配列とポインタ

--- main.cpp
+++ main.cpp
@@ -1,24 +1,20 @@
 #include<stdio.h>
 
-int Recrsive(int n1, int n2);
-
 int main() {
-	int num1, num2;
-	int ans = 0;
+	int array[5] = { 5, 3, 8, 12, 2 };
+	int* p_array = array;
 
-	printf("低を入力：");
-	scanf_s("%d", &num1);
-	printf("指数を入力：");
-	scanf_s("%d", &num2);
-	ans = Recrsive(num1, num2);
-	printf("%dの%d乗は%d", num1, num2, ans);
-	return 0;
-}
-
-int Recrsive(int n1, int n2) {
-	if (n2 <= 1) {
-		return (n1);
+	for (int i = 0; i < 5; i++) {
+		printf("変数array[%d]の値 = %d\n", i, array[i]);
+		printf("変数arrar[%d]のアドレス = %p\n", i, array + i);
 	}
 
-	return (n1 * Recrsive(n1, n2 - 1));
+	printf("-------------------------------------------\n");
+
+	for (int i = 0; i < 5; i++) {
+		printf("変数array[%d]の値 = %d\n", i, *(p_array + i));
+		printf("変数arrar[%d]のアドレス = %p\n", i, p_array + i);
+	}
+
+
 }
