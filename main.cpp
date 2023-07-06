58ddfe580bd59d723f3fb4129de1c5e0187282f0
有佳 小野瀬 <yuuka.082508250825@gmail.com>
2023-07-06 12:01:31 +0900

    単方向リスト

--- main.cpp
+++ main.cpp
@@ -3,37 +3,61 @@
 #include <string.h>
 
 struct Cell {
-	char fishName[32];
+	char name[32];
 	Cell* next;
 };
 
-void create(Cell* endCell, char fishName[]);
-void index(Cell *endCell);
+void create(Cell* endCell, char name[]);
+void index(Cell* endCell, int count);
+void del(Cell* endCell);
 
 int main() {
-	char fishName[32];
+	char name[32];
+	int num;
+	int count = 0;
 	Cell head;
 	head.next = nullptr;
 
 	while (1) {
-		printf("D‚«‚È‚¨ŽõŽi‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
+		printf("[—v‘f‚Ì‘€ì]\n");
+		printf("1.—v‘f‚Ìˆê——•\Ž¦\n");
+		printf("2.ÅŒã”ö‚É—v‘f‚Ì‘}“ü\n");
+		printf("3.ÅŒã”ö‚Ì—v‘f‚Ìíœ\n\n");
+		printf("--------------------------------\n");
+		printf("‘€ì‚ð‘I‘ð‚µ‚Ä‚­‚¾‚³‚¢\n");
+		scanf_s("%d", &num);
 
-		scanf_s("%s", fishName, 32);
+		if (num == 1) {
+			printf("[—v‘f‚Ìˆê——•\Ž¦]\n");
+			index(&head, count);
+		}
+		else if (num == 2) {
+			printf("[ÅŒã”ö‚É—v‘f‚Ì‘}“ü]\n");
+			printf("’Ç‰Á‚·‚é—v‘f‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
+			scanf_s("%s", name, 32);
+			create(&head, name);
+			count++;
+		}
+		else if (num == 3) {
+			printf("[ÅŒã”ö‚Ì—v‘f‚Ìíœ]\n");
+			del(&head);
+			count--;
+		}
 
-		create(&head, fishName);
-
-		index(&head);
 	}
+
+	
 
 	return 0;
 }
 
-void create(Cell* endCell, char fishName[]) {
+void create(Cell* endCell, char name[]) {
+	int num;
 	Cell* newCell;
 
 	newCell = (Cell*)malloc(sizeof(Cell));
 
-	strcpy_s(newCell->fishName, 32, fishName);
+	strcpy_s(newCell->name, 32, name);
 
 	newCell->next = NULL;
 
@@ -42,13 +66,42 @@ void create(Cell* endCell, char fishName[]) {
 	}
 
 	endCell->next = newCell;
+
+	printf("—v‘f\"%s\"‚ªƒŠƒXƒg‚ÌÅŒã”ö‚É‘}“ü‚³‚ê‚Ü‚µ‚½\n\n", name);
+	printf("--------------------------------\n");
+	printf("9.—v‘f‘€ì‚É–ß‚é\n");
+	scanf_s("%d", &num);
+	if (num == 9) {
+		return;
+	}
+
 }
 
-void index(Cell* endCell) {
-	printf("[");
+void index(Cell* endCell, int count) {
+	int no = 0;
+	int num;
+	printf("—v‘f”:%d\n", count);
+
+	printf("—v‘fˆê——:{\n");
 	while (endCell->next != NULL) {
 		endCell = endCell->next;
-		printf("%s,", endCell->fishName);
+		printf("%d:%s,\n", no, endCell->name);
+		no++;
 	}
-	printf("]\n");
+	printf("}\n\n");
+
+	printf("--------------------------------\n");
+	printf("9.—v‘f‘€ì‚É–ß‚é\n");
+	scanf_s("%d", &num);
+	if (num == 9) {
+		return;
+	}
+
+}
+
+void del(Cell* endCell) {
+	while (endCell->next->next != NULL) {
+		endCell = endCell->next;
+	}
+	endCell->next = NULL;
 }
